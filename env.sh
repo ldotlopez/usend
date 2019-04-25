2fbf298b1487a9d5225ad78369faf0865986090a
Luis López <ldotlopez@gmail.com>
2019-04-25 17:46:38 +0200

    Setup environment

---
+++ env.sh
@@ -0,0 +1,10 @@
+if [[ "$(uname -a)" =~ 'Darwin' ]]; then
+	export DYLD_FALLBACK_LIBRARY_PATH="/opt/brew/lib:/usr/lib"
+fi
+
+HKOS_HOME="${HKOS_HOME:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"}"
+HKOS_CONFIG_DIR="${HKOS_CONFIG_DIR:-$HKOS_HOME/config}"
+
+export PYTHONPATH=$HKOS_HOME
+export HKOS_HOME
+export HKOS_CONFIG_DIR
