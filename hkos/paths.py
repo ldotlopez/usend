4dc962af31125a20a373e7110505171d2b03802d
Luis López <ldotlopez@gmail.com>
2019-04-26 15:36:34 +0200

    hkos.paths

---
+++ hkos/paths.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import appdirs
+
+
+if appdirs.system == 'darwin':
+    appname = 'hkOS'
+else:
+    appname = 'hkos'
+
+CONFIG_DIR = os.environ.get(
+    'HKOS_CONFIG_DIR',
+    appdirs.user_config_dir(appname=appname)
+)
+
+DATA_DIR = os.environ.get(
+    'HKOS_DATA_DIR',
+    appdirs.user_data_dir(appname=appname)
+)
