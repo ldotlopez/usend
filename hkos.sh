0544fb49c891575d6cbb87baacbdd1c58756bba0
Luis López <ldotlopez@gmail.com>
2019-05-04 17:17:23 +0200

    Minor fixes on launcher

--- hkos.sh
+++ hkos.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
+
 if [[ "$(uname -a)" =~ 'Darwin' ]]; then
 	export DYLD_FALLBACK_LIBRARY_PATH="/opt/brew/lib:/usr/lib"
 fi
 
-HKOS_HOME="${HKOS_HOME:-"$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"}"
+HKOS_HOME="${HKOS_HOME:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"}"
 HKOS_CONFIG_DIR="${HKOS_CONFIG_DIR:-$HKOS_HOME/config}"
 
 export PYTHONPATH=$HKOS_HOME
@@ -12,5 +14,5 @@ export HKOS_CONFIG_DIR
 if [[ "${BASH_SOURCE[0]}" = "${0}" ]]
 then
 	APP="$1"; shift
-	exec ${PYTHONBIN:-python3} "$HKOS_HOME/apps/${APP}.py" $*
+	exec ${PYTHONBIN:-python3} "$HKOS_HOME/apps/${APP}.py" "$@"
 fi
